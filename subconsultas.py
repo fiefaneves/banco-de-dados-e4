23a3e5c42340df87d750d3d904dc93b5d4b76aea
Eduardo <161507924+Eduardocin@users.noreply.github.com>
2025-07-21 19:57:07 -0300

    refactor: atualizar exemplos

--- subconsultas.py
+++ subconsultas.py
@@ -3,21 +3,6 @@ import sqlite3
 conn = sqlite3.connect('fabrica_chocolate.db')
 cursor = conn.cursor()
 
-# Executando a consulta - Nomes das crianças que não compraram chocolates
-cursor.execute('''
-    SELECT CRI.nome
-    FROM Criança cri
-    LEFT JOIN Chocolate choco ON cri.CPF = choco.CPF_CRIANCA
-    WHERE choco.CPF_CRIANCA IS NULL;
-''')
-
-# Pegando os resultados
-resultados = cursor.fetchall()
-
-# Exibindo os nomes
-for linha in resultados:
-    print(linha[0])
-
 # Subconsulta de linha: Produto mais caro
 cursor.execute('''
     SELECT NOME, PRECO
